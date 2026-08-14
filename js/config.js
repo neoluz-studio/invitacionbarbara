d139e8cf7873fcabb892e5da860d858a8fb741b1
Neoluz Studio <lionelmgaitan24@gmail.com>
2026-08-14 00:16:29 -0300

    Actualiza invitacion Barbara

--- js/config.js
+++ js/config.js
@@ -1,52 +1,52 @@
 window.INVITATION_CONFIG = {
 
     event: {
-        name: "Georgina",
+        name: "Bárbara",
         eyebrow: "MIS XV",
-        date: "2026-09-05T21:00:00-03:00",
-        deadline: "16 DE AGOSTO 2026"
+        date: "2026-10-24T21:30:00-03:00",
+        deadline: "5 DE OCTUBRE 2026"
     },
 
     welcome: {
-        title: "MIS XV Georgina",
+        title: "MIS XV Bárbara",
         subtitle: "QUIERO QUE SEAS PARTE DE ESTE MOMENTO TAN IMPORTANTE PARA MÍ",
         button: "INGRESAR"
     },
 
     texts: {
-        firstPhrase: "Hagamos que sea una noche inolvidable",
-        middlePhrase: "“Hay momentos inolvidables que se atesoran en el corazón para siempre, por esa razón, quiero que compartas conmigo este día tan especial: Mis 15 años.”",
+        firstPhrase: "Una noche muy especial, de esas que se esperan con ilusión y se disfrutan rodeada de las personas que más quiero.",
+        middlePhrase: "Hagamos que sea una noche ¡inolvidable!.",
         closing: "TE ESPERO"
     },
 
     venue: {
-        dateLabel: "05 DE SEPTIEMBRE 2026",
-        timeLabel: "| 21:00 HS |",
-        name: "Salón de eventos FyF",
-        mapsUrl: "https://maps.app.goo.gl/BDX8jmgawNPY4obHA"
+        dateLabel: "24 DE OCTUBRE 2026",
+        timeLabel: "| 21:30 HS |",
+        name: "Club Deportivo Fraga",
+        mapsUrl: "https://www.google.com/maps/search/?api=1&query=Club+Deportivo+Fraga%2C+25+de+Mayo%2C+Maria+del+Carmen+del+Bosco"
     },
 
     dressCode: "Elegante Sport",
 
     gifts: {
         intro: "Nada es más importante que tu presencia, pero si deseas hacerme un presente puedes depositarlo en la siguiente cuenta.",
-        alias: "GEOR.COLOMBERO",
-        holder: "Georgina Colombero"
+        alias: "bar.merlo.mp",
+        holder: "Bárbara Jazmín Merlo Mariani"
     },
 
     social: {
-        hashtag: "#elmejorxv",
+        hashtag: "#MisXVBarbara",
         instagramUrl: "https://instagram.com/"
     },
 
     rsvp: {
         phone: "",
         message: "",
-        endpoint: "https://script.google.com/macros/s/AKfycbxGNt3pso6DDJx-EzvnaaFk3AI0mX1csRpVnMwuNa1HzCqFSP815h-juHPw1B9o48Q8/exec"
+        endpoint: "https://script.google.com/macros/s/AKfycbxXXGuf6f9O6EIZ16SP0WkwYFJaw1EmkxbLm-djiYJwGDdtEcLf0Fv7ILcEj9h-zRJD/exec"
     },
 
     music: {
-        src: "assets/audio/adele.mp3",
+        src: "assets/audio/miley.mp3",
         volume: 0.55
     },
 
@@ -57,7 +57,7 @@ window.INVITATION_CONFIG = {
             "foto2.jpeg",
             "foto3.jpeg",
             "foto4.jpeg",
-            "foto5.jpeg"
+            
         ]
     }
 
